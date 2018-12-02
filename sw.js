25d71b19eb196f6561d20d80c8f398d89d106f67
emilmannfeldt <emil.mannfeldt@gmail.com>
2018-12-02 16:36:21 +0100

    install promt

--- sw.js
+++ sw.js
@@ -6,10 +6,23 @@ if (workbox) {
   console.log(`Boo! Workbox didn't load 😬`);
 }
 //men sparas det till cachen automatiskt eller behöver jag sätta upp precaching?
+/*
 workbox.routing.registerRoute(
   /.*\.(?:js|css|html|png)/,
   workbox.strategies.staleWhileRevalidate()
 );
-
-
+*/
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
 
